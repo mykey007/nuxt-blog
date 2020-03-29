881e030c60e52e5c954de6d4f31b4ec0f1f03c25
Michael Mallowe <mmallowe@gmail.com>
2020-03-28 21:10:20 -0700

    tailwind edits

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-    <header class="grid grid-cols-1 sm:grid-cols-2">
+    <!-- <header class="grid grid-cols-1 sm:grid-cols-2">
         <img class="w-2 rounded-lg md:w-56" src="https://generative-placeholders.glitch.me/image?width=600&height=300&style=cubic-disarray">
         <ul>
             <li>
@@ -7,6 +7,47 @@
                 <nuxt-link to="/users">Users</nuxt-link>
             </li>
         </ul>
-    </header>
+    </header> -->
+
+    <div class="font-sans antialiased" id="app">
+        <nav class="flex items-center justify-between flex-wrap bg-cyan p-6">
+            <div class="flex items-center flex-no-shrink text-white mr-6">
+            <!-- <img class="h-8 w-8 mr-2 rounded-lg md:w-56" src="https://generative-placeholders.glitch.me/image?width=600&height=300&style=cubic-disarray"> -->
+            <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
+            <span class="font-semibold text-xl tracking-tight">Tailwind CSS</span>
+            </div>
+            <div class="block sm:hidden">
+            <button @click="toggle" class="flex items-center px-3 py-2 border rounded text-teal-lighter border-teal-light hover:text-white hover:border-white">
+                <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+            </button>
+            </div>
+            <div :class="open ? 'block': 'hidden'" class="w-full flex-grow sm:flex sm:items-center sm:w-auto">
+            <div class="text-sm sm:flex-grow">
+               <nuxt-link to="/">Home</nuxt-link>
+                <nuxt-link to="/users">Users</nuxt-link>
+            </div>
+            <div>
+                <a href="#" class="no-underline inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal hover:bg-white mt-4 sm:mt-0">Call Me!</a>
+            </div>
+            </div>
+        </nav>
+    </div>
     
 </template>
+
+<script>
+export default {
+     data: function() {
+         return  {
+           open: false,
+         }
+    },
+    methods: {
+        toggle() {
+            this.open = !this.open
+        }
+    }
+}
+
+
+</script>
---
+++ pages/posts/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <main>
+    <div class="container mx-auto">
+      <div class="md:flex">
+        <div class="md:flex-shrink-0">
+          <img class="" src="https://generative-placeholders.glitch.me/image?width=600&height=300&style=joy-division" alt="joy division">
+          
+        </div>
+        <div class="fader mt-4 md:mt-0 md:ml-6">
+          <div class="uppercase tracking-wide text-sm text-indigo-600 font-bold">Post-Punk</div>
+          <nuxt-link to="/users" class="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline">Users</nuxt-link>
+          <p class="mt-2 text-gray-600">Getting a new business off the ground is a lot of hard work. Here are five ideas you can use to find your first customers.</p>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
